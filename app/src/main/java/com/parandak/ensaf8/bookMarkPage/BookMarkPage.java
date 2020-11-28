55ad11654f374a82fc8c06ef5ae0edca0f0ecefe
ali parandak <6592249-aliparandak@users.noreply.gitlab.com>
2020-11-28 14:14:16 +0330

    btn_add_folder_bookmarkToBookMarkPage

--- app/src/main/java/com/parandak/ensaf8/bookMarkPage/BookMarkPage.java
+++ app/src/main/java/com/parandak/ensaf8/bookMarkPage/BookMarkPage.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -55,10 +56,8 @@ public class BookMarkPage extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.book_mark_page, container, false);
-
-
-
         toolbar = view.findViewById(R.id.toolbar);
+
         return view;
     }
 
@@ -78,9 +77,8 @@ public class BookMarkPage extends DialogFragment {
         mcontext = null;
         mactivity = null;
     }
-
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -115,25 +113,50 @@ public class BookMarkPage extends DialogFragment {
                         builderInner.show();
                         break;
                     case R.id.action_add_phone:
-                        //Toast.makeText(getContext(),"adding phone number", Toast.LENGTH_SHORT).show();
-                        LayoutInflater layoutInflater = LayoutInflater.from(mcontext);
-                        View viewAddPhone = layoutInflater.inflate(R.layout.add_phone_num,null);
-                        final EditText edi_add_phone = viewAddPhone.findViewById(R.id.edi_add_phone);
-                        AlertDialog.Builder builderAddPhone = new AlertDialog.Builder(getContext());
-                        builderAddPhone.setMessage("ADDPhoneNumber" );
-                        builderAddPhone.setView(viewAddPhone);
-                        builderAddPhone.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                            }
-                        });
-                        builderAddPhone.show();
-                        break;
-                }
+                        addPhoneNumDialog();
+                        break;                }
                 //FullDialog.this.dismiss();
                 return true;
             }
         });
+        Button btn_add_folder_bookmark = view.findViewById(R.id.btn_add_folder_bookmark);
+        btn_add_folder_bookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mcontext,"adding bookMarkFolder!!!", Toast.LENGTH_SHORT).show();
+                //addBookMarkFolderDialog();
+            }
+        });
+    }
+    private void addPhoneNumDialog(){
+        //Toast.makeText(getContext(),"adding phone number", Toast.LENGTH_SHORT).show();
+        LayoutInflater layoutInflater = LayoutInflater.from(mcontext);
+        View viewAddPhone = layoutInflater.inflate(R.layout.add_phone_num,null);
+        final EditText edi_add_phone = viewAddPhone.findViewById(R.id.edi_add_phone);
+        AlertDialog.Builder builderAddPhone = new AlertDialog.Builder(getContext());
+        builderAddPhone.setMessage("ADDPhoneNumber" );
+        builderAddPhone.setView(viewAddPhone);
+        builderAddPhone.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builderAddPhone.show();
+    }
+    private void addBookMarkFolderDialog(){
+        LayoutInflater layoutInflater = LayoutInflater.from(mcontext);
+        View viewAddBookFolder = layoutInflater.inflate(R.layout.add_book_folder,null);
+        final EditText edi_add_book_folder = viewAddBookFolder.findViewById(R.id.edi_add_book_folder);
+        AlertDialog.Builder builderAddBookmarkFolder = new AlertDialog.Builder(mcontext);
+        builderAddBookmarkFolder.setMessage("ADDBookMarkFolder" );
+        builderAddBookmarkFolder.setView(edi_add_book_folder);
+        builderAddBookmarkFolder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"add FOLDER : " + edi_add_book_folder.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        builderAddBookmarkFolder.show();
     }
 }
