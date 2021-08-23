665d0836a82b22632e719ef1f19b7c41690ed042
unknown <aliparandak1990@gmail.com.com>
2021-08-23 21:58:39 +0430

    bookQueryString

--- app/src/main/java/com/parandak/ensaf8/bookMarkPage/BookMarkPageQuery.java
+++ app/src/main/java/com/parandak/ensaf8/bookMarkPage/BookMarkPageQuery.java
@@ -9,6 +9,7 @@ import com.parandak.ensaf8.dataBase.model_Indivi.BookMark;
 import com.parandak.ensaf8.dataBase.model_Indivi.BookMarkType;
 import com.parandak.ensaf8.dataBase.model_Indivi.Cons_Phase;
 import com.parandak.ensaf8.dataBase.model_Indivi.GPoint;
+import com.parandak.ensaf8.dataBase.model_Indivi.Indi_Coop;
 import com.parandak.ensaf8.dataBase.model_Indivi.Indi_Geop;
 import com.parandak.ensaf8.dataBase.model_Indivi.Individual;
 
@@ -18,9 +19,25 @@ public class BookMarkPageQuery {
     public String getIndiIDBookMark(String B_type_id){
         return "SELECT " + BookMark.KEY_IndID
                 + " FROM " + BookMark.TABLE
-                + " WHERE " + BookMark.KEY_B_TYPE_ID + " = " + B_type_id;
-        //        + " ORDER BY " + BookMark.KEY_IndID + " ASC";
+                + " WHERE " + BookMark.KEY_B_TYPE_ID + " = " + B_type_id
+                + " ORDER BY " + BookMark.KEY_IndID + " ASC";
     }
+
+
+    public String getIndiCoopBookMark(String B_type_id){
+        return " SELECT "
+                + Indi_Coop.TABLE + "." + Indi_Coop.KEY_FirstPartID + " , "
+                + Indi_Coop.TABLE + "." + Indi_Coop.KEY_Title + " , "
+                + Individual.TABLE + "." + Individual.KEY_ID_Indi + " , "
+                + Individual.TABLE + "." + Individual.KEY_IndiName
+                + " FROM "
+                + Indi_Coop.TABLE
+                + " INNER JOIN " + Individual.TABLE
+                + " ON " +  Individual.TABLE + "." + Individual.KEY_ID_Indi+ " = " + Indi_Coop.TABLE + "." + Indi_Coop.KEY_SecondPartID
+                + " WHERE " + Indi_Coop.TABLE + "." + Indi_Coop.KEY_FirstPartID + " IN ("
+                    + getIndiIDBookMark(B_type_id) + ")";
+    }
+
 
     public String getIndiBookMark(String B_type_id){
         return "SELECT " + Individual.TABLE + "." + Individual.KEY_ID_Indi + ","
@@ -28,8 +45,8 @@ public class BookMarkPageQuery {
                 + Individual.TABLE + "." + Individual.KEY_IsCons
                 + " FROM " + Individual.TABLE
                 + " WHERE " + Individual.TABLE + "." + Individual.KEY_ID_Indi + " IN "
-                + "(" + getIndiIDBookMark(B_type_id) + ")"
-                + " ORDER BY " + Individual.TABLE + "." + Individual.KEY_ID_Indi + " ASC";
+                + "(" + getIndiIDBookMark(B_type_id) + ")";
+        //        + " ORDER BY " + Individual.TABLE + "." + Individual.KEY_ID_Indi + " ASC";
         //        + " ORDER BY " + BookMark.KEY_IndID + " ASC";
     }
 
@@ -43,20 +60,20 @@ public class BookMarkPageQuery {
                 + " ON " + Indi_Geop.TABLE + "." + Indi_Geop.KEY_GeopID
                 + " = " + GPoint.TABLE + "." + GPoint.KEY_IDGeop
                 + " WHERE " + Indi_Geop.TABLE + "." + Indi_Geop.KEY_IndiID + " IN "
-                + "(" + getIndiIDBookMark(B_type_id) + ")"
-                + " ORDER BY " + Indi_Geop.TABLE + "." + Indi_Geop.KEY_IndiID + " ASC";
+                + "(" + getIndiIDBookMark(B_type_id) + ")";
+                //+ " ORDER BY " + Indi_Geop.TABLE + "." + Indi_Geop.KEY_IndiID + " ASC";
 
     }
 
     public String getPhaseBookMark(String B_type_id){
-        return "SELECT " + Cons_Phase.TABLE + "." + Cons_Phase.KEY_ID_Cons_Phase + ","
-                + Cons_Phase.TABLE + "." + Cons_Phase.KEY_IndID + ","
+        return "SELECT " + Cons_Phase.TABLE + "." + Cons_Phase.KEY_IndID + ","
+                + Cons_Phase.TABLE + "." + Cons_Phase.KEY_ID_Cons_Phase + ","
                 + Cons_Phase.TABLE + "." + Cons_Phase.KEY_Phase + ","
                 + Cons_Phase.TABLE + "." + Cons_Phase.KEY_PhaseDate
                 + " FROM " + Cons_Phase.TABLE
                 + " WHERE " + Cons_Phase.TABLE + "." + Cons_Phase.KEY_IndID + " IN "
-                + "(" + getIndiIDBookMark(B_type_id) + ")"
-                + " ORDER BY " + Cons_Phase.TABLE + "." + Cons_Phase.KEY_IndID + " ASC";
+                + "(" + getIndiIDBookMark(B_type_id) + ")";
+                //+ " ORDER BY " + Cons_Phase.TABLE + "." + Cons_Phase.KEY_IndID + " ASC";
 
     }
 
