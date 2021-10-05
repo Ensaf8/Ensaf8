12a5b514748f018f1ec217419885683e5bd89026
unknown <aliparandak1990@gmail.com.com>
2021-10-05 14:36:39 +0330

    24HHdateFormat

--- app/src/main/java/com/parandak/ensaf8/dataBase/DataContract.java
+++ app/src/main/java/com/parandak/ensaf8/dataBase/DataContract.java
@@ -1,5 +1,5 @@
 package com.parandak.ensaf8.dataBase;
 
 public class DataContract {
-    public static String dateFormat = "yyyy-MM-dd hh:mm:ss";
+    public static String dateFormat = "yyyy-MM-dd HH:mm:ss";
 }
