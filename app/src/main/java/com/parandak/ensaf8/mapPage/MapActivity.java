cd453d29174973437896a91b63ac72166d9f86c0
Ali <aliparandak1990@gmail.com>
2022-03-16 19:48:42 +0330

    someIdea!

--- app/src/main/java/com/parandak/ensaf8/mapPage/MapActivity.java
+++ app/src/main/java/com/parandak/ensaf8/mapPage/MapActivity.java
@@ -597,6 +597,7 @@ public class MapActivity extends BaseActivity implements ItemizedIconOverlay.OnI
             public void onClick(View v) {
                 if (isConnected){
                     AddReminderDialouge addReminderDialouge = new AddReminderDialouge(context,activity);
+                    mLocationOverlay.getMyLocation()
                     addReminderDialouge.showDialogueADD(ID_CONS_SELECTED);
                 } else {
                     Toast.makeText(getApplicationContext(),"SignIn First !"  , Toast.LENGTH_SHORT).show();
