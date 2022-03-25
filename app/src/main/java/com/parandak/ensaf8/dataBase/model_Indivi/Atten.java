aebd18abc0765b0b0e34b8ed18a33ea8da43a1d4
Ali <aliparandak1990@gmail.com>
2022-03-25 16:33:56 +0430

    addAttenModel

---
+++ app/src/main/java/com/parandak/ensaf8/dataBase/model_Indivi/Atten.java
@@ -0,0 +1,48 @@
+package com.parandak.ensaf8.dataBase.model_Indivi;
+
+public class Atten {
+
+    public static final String TABLE = "Atten";
+
+    public static final String KEY_ID_Tend = "ID_Atten";
+    public static final String KEY_Ind1ID = "ind1ID";
+    public static final String KEY_Ind2ID = "ind2ID";
+    public static final String KEY_AttenDate = "attenDate";
+
+    private String ID_Atten;
+    private String ind1ID ;
+    private String ind2ID ;
+    private String attenDate ;
+
+    public String getID_Atten() {
+        return ID_Atten;
+    }
+
+    public void setID_Atten(String ID_Atten) {
+        this.ID_Atten = ID_Atten;
+    }
+
+    public String getInd1ID() {
+        return ind1ID;
+    }
+
+    public void setInd1ID(String ind1ID) {
+        this.ind1ID = ind1ID;
+    }
+
+    public String getInd2ID() {
+        return ind2ID;
+    }
+
+    public void setInd2ID(String ind2ID) {
+        this.ind2ID = ind2ID;
+    }
+
+    public String getAttenDate() {
+        return attenDate;
+    }
+
+    public void setAttenDate(String attenDate) {
+        this.attenDate = attenDate;
+    }
+}
---
+++ app/src/main/java/com/parandak/ensaf8/dataBase/model_Indivi/repo_Indi/AttenRepo.java
@@ -0,0 +1,59 @@
+package com.parandak.ensaf8.dataBase.model_Indivi.repo_Indi;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.parandak.ensaf8.dataBase.DatabaseManager;
+import com.parandak.ensaf8.dataBase.model_Indivi.Atten;
+import com.parandak.ensaf8.dataBase.model_Indivi.Individual;
+
+
+
+public class AttenRepo {
+    Atten atten;
+
+    public AttenRepo(){
+        this.atten = new Atten();
+    }
+
+    public static String createTable(){
+        return "CREATE TABLE IF NOT EXISTS "+ Atten.TABLE+" ("
+                + Atten.KEY_ID_Tend +" INTEGER "+" , "
+                + Atten.KEY_Ind1ID +" INTEGER "+" , "
+                + Atten.KEY_Ind2ID +" INTEGER "+" , "
+                + Atten.KEY_AttenDate +" TEXT "+" , "
+                + " PRIMARY KEY(" + Atten.KEY_ID_Tend + ")"
+                + " FOREIGN KEY(" + Atten.KEY_Ind1ID + ")"
+                + " REFERENCES " + Individual.TABLE + " ( " + Individual.KEY_ID_Indi + ") ON DELETE CASCADE " + ","
+                + " FOREIGN KEY(" + Atten.KEY_Ind2ID + ")"
+                + " REFERENCES " + Individual.TABLE + " ( " + Individual.KEY_ID_Indi + ") ON DELETE CASCADE "
+                +");";
+
+    }
+
+    public int insert (Atten atten){
+        int attenId;
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Atten.KEY_Ind1ID,atten.getInd1ID());
+        values.put(Atten.KEY_Ind2ID,atten.getInd2ID());
+        values.put(Atten.KEY_AttenDate,atten.getAttenDate());
+        attenId = (int) db.insert(Atten.TABLE,null,values);
+        DatabaseManager.getInstance().closeDatabase();
+        return attenId;
+    }
+
+
+    public void delete(){
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        db.delete(Atten.TABLE,null,null);
+        DatabaseManager.getInstance().closeDatabase();
+    }
+
+    public boolean deleteID_Atten(String ID_Atten){
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        boolean b = db.delete(Atten.TABLE,Atten.KEY_ID_Tend + "=?",new String[]{ID_Atten})>0;
+        DatabaseManager.getInstance().closeDatabase();
+        return b;
+    }
+}
